cd05a3ee668c318d902abf2c523b155a27bc81e1
Jeff Jagoda <jeffrey.jagoda@gmail.com>
2014-07-25 10:50:45 -0400

    Log helper.

---
+++ test/Logger_spec.js
@@ -0,0 +1,23 @@
+"use strict";
+var Lab = require("lab");
+
+var describe = Lab.describe;
+var it       = Lab.it;
+
+describe("A Logger", function () {
+	describe("without any tags", function () {
+		it("can create a formatted 'info' message");
+
+		it("can create a formatted 'warn' message");
+
+		it("can create a formatted 'error' message");
+	});
+
+	describe("with tags", function () {
+		it("can create a formatted 'info' message");
+
+		it("can create a formatted 'warn' message");
+
+		it("can create a formatted 'error' message");
+	});
+});
